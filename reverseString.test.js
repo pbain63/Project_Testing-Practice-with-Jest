2670c9313c87d23f301ff96eb475369a3099d3c4
pbain63 <prodipbain63@gmail.com>
2026-08-25 09:55:57 +0600

    Add another Jest test to function for passing in reverseString.test.js

--- reverseString.test.js
+++ reverseString.test.js
@@ -3,3 +3,7 @@ const reverseString = require("./reverseString.js");
 test("takes a string and returns it reversed", () => {
   expect(reverseString("abcdef")).toBe("fedcba");
 });
+
+test("takes a string and returns it reversed", () => {
+  expect(reverseString("abc de ;) 🌿")).toBe("🌿 ); ed cba");
+});
