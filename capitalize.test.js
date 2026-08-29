3340e44c88b2caceca580565452163a36af6d943
pbain63 <prodipbain63@gmail.com>
2026-08-29 13:04:12 +0600

    Add another Jest test to function for pass in capitalize.test.js

--- capitalize.test.js
+++ capitalize.test.js
@@ -3,3 +3,7 @@ const capitalize = require("./capitalize.js");
 test("takes string and returns first character capitalized", () => {
   expect(capitalize("Abcdef")).toBe("Abcdef");
 });
+
+test("takes string and returns first character capitalized", () => {
+  expect(capitalize(" ")).toBe(" "); // TODO
+});
