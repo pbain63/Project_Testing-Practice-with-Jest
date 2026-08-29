1cfb69ee9e86fb2cc8d37869952c0871b19ac694
pbain63 <prodipbain63@gmail.com>
2026-08-29 21:27:58 +0600

    Add Jest test to subtract two numbers for pass in calculator.test.js

--- calculator.test.js
+++ calculator.test.js
@@ -4,3 +4,9 @@ test("adds two numbers correctly", () => {
   expect(calculator.add(1, 2)).toBe(3);
   expect(calculator.add(-3, 18)).toBe(15);
 });
+
+test("subtracts two number correctly", () => {
+  expect(calculator.subtract(8, 3)).toBe(5);
+  expect(calculator.subtract(-3, -9)).toBe(6);
+});
+
