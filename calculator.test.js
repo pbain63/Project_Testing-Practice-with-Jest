500f5bd1b693e219f015a1a1fd623de44b0e3f91
pbain63 <prodipbain63@gmail.com>
2026-08-30 22:40:30 +0600

    Put all the tests into describe as a whole in calculator.test.js

--- calculator.test.js
+++ calculator.test.js
@@ -1,25 +1,27 @@
 const calculator = require("./calculator");
 
-test("adds two numbers correctly", () => {
-  expect(calculator.add(1, 2)).toBe(3);
-  expect(calculator.add(-3, 18)).toBe(15);
-});
+describe("calculator", () => {
+  test("adds two numbers correctly", () => {
+    expect(calculator.add(1, 2)).toBe(3);
+    expect(calculator.add(-3, 18)).toBe(15);
+  });
 
-test("subtracts two number correctly", () => {
-  expect(calculator.subtract(8, 3)).toBe(5);
-  expect(calculator.subtract(-3, -9)).toBe(6);
-});
+  test("subtracts two number correctly", () => {
+    expect(calculator.subtract(8, 3)).toBe(5);
+    expect(calculator.subtract(-3, -9)).toBe(6);
+  });
 
-test("multiplies two numbers correctly", () => {
-  expect(calculator.multiply(5, -7)).toBe(-35);
-  expect(calculator.multiply(8, 0)).toBe(0);
-});
+  test("multiplies two numbers correctly", () => {
+    expect(calculator.multiply(5, -7)).toBe(-35);
+    expect(calculator.multiply(8, 0)).toBe(0);
+  });
 
-test("divides two numbers correctly", () => {
-  expect(calculator.divide(12, 4)).toBe(3);
-  expect(calculator.divide(13, -2)).toBe(-6.5);
-});
+  test("divides two numbers correctly", () => {
+    expect(calculator.divide(12, 4)).toBe(3);
+    expect(calculator.divide(13, -2)).toBe(-6.5);
+  });
 
-test("throws an error when dividing by zero", () => {
-  expect(() => calculator.divide(12, 0)).toThrow(Error);
+  test("throws an error when dividing by zero", () => {
+    expect(() => calculator.divide(12, 0)).toThrow(Error);
+  });
 });
