398523d22741414e5d339362fa1a0bec09992e40
pbain63 <prodipbain63@gmail.com>
2026-08-31 21:07:33 +0600

    Add wrapping describe around test cases in reverseString.test.js

--- reverseString.test.js
+++ reverseString.test.js
@@ -1,13 +1,15 @@
 const reverseString = require("./reverseString.js");
 
-test("takes a string and returns it reversed", () => {
-  expect(reverseString("abcdef")).toBe("fedcba");
-});
+describe("reverseString", () => {
+  test("takes a string and returns it reversed", () => {
+    expect(reverseString("abcdef")).toBe("fedcba");
+  });
 
-test("takes a string and returns it reversed", () => {
-  expect(reverseString("abc de ;) 🌿")).toBe("🌿 ); ed cba");
-});
+  test("takes a string and returns it reversed", () => {
+    expect(reverseString("abc de ;) 🌿")).toBe("🌿 ); ed cba");
+  });
 
-test("takes a string and returns it reversed", () => {
+  test("takes a string and returns it reversed", () => {
     expect(reverseString(" ")).toBe(" ");
   });
+});
