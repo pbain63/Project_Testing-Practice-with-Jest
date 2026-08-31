49c4eb5011eea1fada5f4eb79aeb0d80c67f9ba7
pbain63 <prodipbain63@gmail.com>
2026-08-31 14:04:15 +0600

    Add test to pass in analyzeArray.test.js

---
+++ analyzeArray.test.js
@@ -0,0 +1,12 @@
+const analyzeArray = require("./analyzeArray");
+
+describe("analyzeArray", () => {
+  test("returns the average, minimum, maximum and length", () => {
+    expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+      average: 4,
+      min: 1,
+      max: 8,
+      length: 6,
+    });
+  });
+});
